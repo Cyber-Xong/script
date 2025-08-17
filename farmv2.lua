23c656912fa911d81fe6e06042cc5cad9dff6ea9
xing-xong <inviter149@gmail.com>
2025-08-17 11:26:13 +0200

    Update farmv2.lua

--- farmv2.lua
+++ farmv2.lua
@@ -59,15 +59,15 @@ function startAutoFarm()
                     -- choisir une pièce aléatoire
                     local coinToCollect = validCoins[math.random(1, #validCoins)]
                     humPart.CFrame = coinToCollect.CFrame
-                    task.wait(1) -- temps de ramassage selon device
+                    task.wait(1.2) -- temps de ramassage selon device
                     humPart.CFrame = CFrame.new(132, 140, 60) + Vector3.new(0, 3, 0) -- retour
                     task.wait(isMobile and 1.5 or 1.3) -- temps de retour selon device
                 else
                     humPart.CFrame = CFrame.new(132, 140, 60) + Vector3.new(0, 3, 0)
-                    task.wait(1.5)
+                    task.wait(2)
                 end
             else
-                task.wait(1)
+                task.wait(1.5)
             end
         end
     end)
